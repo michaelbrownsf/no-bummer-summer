f4df99fd7cc636c9ab55f4080dfb0044f60cca2c
cecyc <cecycorrea@gmail.com>
2015-06-19 11:23:06 -0500

    user activities autocreated when on User.create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @activities = Activity.all
     @user = User.create(user_params)
+    @user_activity = UserActivity.create(
+      [{user_id: @user.id, activity_id: 1, completed: false},
+        {user_id: @user.id, activity_id: 2, completed: false},
+        {user_id: @user.id, activity_id: 3, completed: false},
+        {user_id: @user.id, activity_id: 4, completed: false},
+        {user_id: @user.id, activity_id: 5, completed: false},
+        {user_id: @user.id, activity_id: 6, completed: false},
+        {user_id: @user.id, activity_id: 7, completed: false},
+        {user_id: @user.id, activity_id: 8, completed: false},
+        {user_id: @user.id, activity_id: 9, completed: false},
+        {user_id: @user.id, activity_id: 10, completed: false}
+        ])
+
+
     respond_to do |format|
       if @user.save
         format.json { render json: @user, status: :created, location: @user }
