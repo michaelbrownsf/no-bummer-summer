311eccfaf8026329594838fecf2973bdb87747d8
michaelbrownsf <michaelbrownsf@gmail.com>
2015-06-20 16:54:59 -0500

    added enclosing div for Footer Component

--- app/assets/javascripts/components/FooterComponent.js.jsx
+++ app/assets/javascripts/components/FooterComponent.js.jsx
@@ -1,9 +1,11 @@
 var LoginPage = React.createClass({
 	render: function() {
 		return (
+			<div>
 			<div className="line"></div>
 			<footer><h6>an Iron Yard Hackathon Project</h6></footer>
 			<div className="line"></div>
+			</div>
 		);
 	}
 });
