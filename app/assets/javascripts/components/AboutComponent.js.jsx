447b1f4a39795b77084fadd650289e7c83b0eeb8
michaelbrownsf <michaelbrownsf@gmail.com>
2015-06-20 16:20:37 -0500

    Added content to About Component

--- app/assets/javascripts/components/AboutComponent.js.jsx
+++ app/assets/javascripts/components/AboutComponent.js.jsx
@@ -2,9 +2,19 @@
 var AboutPage = React.createClass({
 	render: function() {
 		return (
-			<p>
-				this is the about paragraph
-			</p>
+			<div class="col12">
+<h1>What is No Bummer Summer?</h1>
+<p>Let’s face it: As adults, summer is a bit of a letdown. You spend the majority of your youthful years counting down the days until that long awaited dismissal bell, granting you seemingly endless days of popsicles, bike rides, water parks and too much television. But college graduation comes and goes, and carefree fun is replaced with worrying about PTO and sick leave.</p>
+<p>One Austinite is out to help you make the most of your summer, regardless of the abundance of adulthood’s obligations. Inspired by the spirit of her friend Esme Barrera, Kerissa Bearce started “No Bummer Summer,” a tribute to the catchphrase of her late friend, in 2012. “Esme liked to encompass all those things and I think she was the perfect example of being an adult, but knowing how to have fun with the wonder of a child.”
+Following a summer trip to Inks Lake after Barrera’s tragic passing on New Years Day of 2012, Bearce and friends had an idea. “High off the adrenaline of jumping off cliffs and side-stepping leeches, we came up with the idea of having a No Bummer Summer punch card. I asked on Facebook what kind of things were quintessential summer activities, and by the end of it we had 20 ideas.”</p>
+<p>Bearce then took to her computer, and within days, the punch cards were in the mail and the No Bummer Summer was underway.</p>
+
+<p>With activities spanning from eating a sno cone and taking a night swim to day drinking and watching an outside movie, the card offers inspiration for those looking to shake their summer blues. During the first summer, Bearce saw an explosion amongst online participants, mostly comprised of her social media savvy friends. “I knew my friends would take to Instagram to document the No Bummer Summers they were having. There were three pictures tagged on Instagram with #nobummersummer at the beginning of June and well over 600 at the end of August.”</p>
+
+<p>Alexa Schereffler, Cessy Correa and Michael Brown (all students at the Iron Yard, a code school boot camp) decided to take this idea a little further. The trio designed and built an app/web portal to log in to and track your process with the list.</p>
+</div>
+<!-- End Content Area -->
+
 		);
 	},
 
