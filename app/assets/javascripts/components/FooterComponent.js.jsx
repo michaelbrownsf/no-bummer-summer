e7bb92e6cd48ced02e71631f69aa2f7ec474ea23
michaelbrownsf <michaelbrownsf@gmail.com>
2015-06-20 16:48:38 -0500

    Added HTML to Footer Component

--- app/assets/javascripts/components/FooterComponent.js.jsx
+++ app/assets/javascripts/components/FooterComponent.js.jsx
@@ -1,9 +1,9 @@
 var LoginPage = React.createClass({
 	render: function() {
 		return (
-			<div>
-				put your html here for the footer
-			</div>
+			<div class="line"></div>
+			<footer><h6>an Iron Yard Hackathon Project</h6></footer>
+			<div class="line"></div>
 		);
 	}
 });
