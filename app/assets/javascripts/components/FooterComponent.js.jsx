0a7cb6b4a5d734db0e609787b164b89d17df4e68
michaelbrownsf <michaelbrownsf@gmail.com>
2015-06-20 16:53:02 -0500

    changed Class to className in footer

--- app/assets/javascripts/components/FooterComponent.js.jsx
+++ app/assets/javascripts/components/FooterComponent.js.jsx
@@ -1,9 +1,9 @@
 var LoginPage = React.createClass({
 	render: function() {
 		return (
-			<div class="line"></div>
+			<div className="line"></div>
 			<footer><h6>an Iron Yard Hackathon Project</h6></footer>
-			<div class="line"></div>
+			<div className="line"></div>
 		);
 	}
 });
