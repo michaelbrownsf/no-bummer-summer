e78b0e4a18c2995fe6d206b4ad749e606d0c4ed2
Alexa Schreffler <alexa.schreffler@gmail.com>
2015-06-20 18:22:48 -0500

    replaced some text inputs with password input and changed .css appropriately

--- app/assets/javascripts/components/LoginComponent.js.jsx
+++ app/assets/javascripts/components/LoginComponent.js.jsx
@@ -1,11 +1,10 @@
-var User = new UserModel();
 
 var LoginPage = React.createClass({
 	render: function() {
 		return (
 			<form ref="loginForm" onSubmit={this.userLogin}>
-				<input type="text" ref="username" placeholder="username" />
-				<input type="text" ref="password" placeholder="password" />
+				<input type="text" ref="email" placeholder="email" /><br />
+				<input type="password" ref="password" placeholder="password" /><br />
 				<button type="submit" ref="loginBtn">Log In</button>
 			</form>
 		);
@@ -13,7 +12,11 @@ var LoginPage = React.createClass({
 
 	userLogin: function(e) {
 		e.preventDefault;
+		var loginValues = {
+			email: this.refs.email.getDOMNode.value,
+			password: this.refs.password.getDOMNode.value
+		}
 		
-		User.fetch();
+		$.post('https://no-bummer-summer-2015.herokuapp.com/login', {email: loginValues.email, password: loginValues.password})
 	}
 });
--- app/assets/javascripts/components/SignupComponent.js.jsx
+++ app/assets/javascripts/components/SignupComponent.js.jsx
@@ -1,13 +1,14 @@
+var newListUser = new UserCollection();
 
 var SignupPage = React.createClass({
 	render: function() {
 		return (
-			<form onSubmit={submitNewUser}>
-				<input type="text" className="newName" ref="newFirst" placeholder="first name" />
-				<input type="text" className="newName" ref="newLast" placeholder="last name" />
-				<input type="text" className="newEmail" ref="newEmail" placeholder="enter email" />
-				<input type="text" className="newPassword" ref="newPassword" placeholder="enter password" />
-				<input type="text" className="confirmPassword" ref="confirmPassword" placeholder="confirm password" />
+			<form className="centeredcontainer" onSubmit={this.submitNewUser}>
+				<input type="text" className="newName" ref="newFirst" placeholder="first name" /><br />
+				<input type="text" className="newName" ref="newLast" placeholder="last name" /><br />
+				<input type="text" className="newEmail" ref="newEmail" placeholder="enter email" /><br />
+				<input type="password" className="newPassword" ref="newPassword" placeholder="enter password" /><br />
+				<input type="password" className="confirmPassword" ref="confirmPassword" placeholder="confirm password" /><br />
 				<button type="submit">Submit</button>
 			</form>
 		);
@@ -15,17 +16,18 @@ var SignupPage = React.createClass({
 
 	submitNewUser: function(e) {
 		e.preventDefault();
-		console.log('you did it');
 
 		var newUser = new UserModel ({
-			firstName: this.refs.newFirst.value(),
-			lastName: this.refs.newLast.value(),
-			email: this.refs.email.value(),
-			password: this.refs.password.value(),
-			password_confirmation: this.refs.password.value()
+			firstName: this.refs.newFirst.getDOMNode.value,
+			lastName: this.refs.newLast.getDOMNode.value,
+			email: this.refs.newEmail.getDOMNode.value,
+			password: this.refs.newPassword.getDOMNode.value,
+			password_confirmation: this.refs.newPassword.getDOMNode.value
 		});
 
-		newUser.save();
+		console.log(newUser);
+		
+
 	}
 
 
